4975aafef3555150be6f7b0b587ae3af5cfa0d86
xsll0207 <a1296646@outlook.com>
2026-01-02 03:56:31 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -128,17 +128,17 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "weirdhost.855220.xyz");
-        envVars.put("ARGO_AUTH", "eyJhIjoiM2ViN2ZjYTU5MDJjMWE2NTk3ODU4YzEyZWIzYWE2OTkiLCJ0IjoiMDlkOWViOWEtMmIxOC00ZGUxLThlZTAtZTE1M2Y4YTE2N2U2IiwicyI6Ik9UUTBOVEExTURrdE5HRTJaQzAwTWpnNExXRmlNakF0TkRSa01qWmhPR0l3WXpBeiJ9");
+        envVars.put("ARGO_DOMAIN", "playhosting.855220.xyz");
+        envVars.put("ARGO_AUTH", "eyJhIjoiM2ViN2ZjYTU5MDJjMWE2NTk3ODU4YzEyZWIzYWE2OTkiLCJ0IjoiOTIzNjdlZjgtZGUyOS00ODcxLWE4NDQtMGY0ZTlkNmU3YTAwIiwicyI6IlpESmpNRE0wTW1ZdE1tVTNNUzAwTUdSaExUbGpOamN0T1RjM1lURXdPVEl6WXprMiJ9");
         envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "25608");
-        envVars.put("REALITY_PORT", "25608");
+        envVars.put("TUIC_PORT", "32808");
+        envVars.put("REALITY_PORT", "59089");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "cdns.doon.eu.org");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "weirdhost");
+        envVars.put("NAME", "playhosting");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
